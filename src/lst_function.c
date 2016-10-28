eaf96ddbdb9a1518c4f2d97166c0597af2d8dde4
Aymeric MAITRE <amaitre@e2r4p8.42.fr>
2016-10-28 16:58:27 +0200

    Get name comment ok

--- src/lst_function.c
+++ src/lst_function.c
@@ -14,13 +14,13 @@
 
 header2_t	*cw_add_champ_to_lst(t_cwdata *data)
 {
-	header2_t	new_champion_node;
+	header2_t	new_node;
 	t_list		*champion_node;
 
 
-	new_champion_node.prog_name = ft_strnew(0);
-	new_champion_node.comment = ft_strnew(0);
-	champion_node = ft_lstnew((void *)&new_champion_node, sizeof(new_champion_node));
+	new_node.prog_name = ft_strnew(0);
+	new_node.comment = ft_strnew(0);
+	champion_node = ft_lstnew((void *)&new_node, sizeof(new_node));
 
 	ft_lstadd_push_back(&data->beginlist, champion_node);
 
