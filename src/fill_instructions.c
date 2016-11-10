30d26a99da1a56c5563db2903ee422ac083b7585
David MATHE <dmathe@e1r6p15.42.fr>
2016-11-10 21:07:23 +0100

    fill instruction

--- src/fill_instructions.c
+++ src/fill_instructions.c
@@ -14,13 +14,21 @@
 
 int			check_encodage(int opcode)
 {
-	if (opcode == 1 || opcode == 2 || opcode == 6 || opcode == 6 || \
+	if (opcode == 1 || opcode == 2 || opcode == 6 || \
 		opcode == 7 || opcode == 8 || opcode == 13)
 		return (4);
 	else if (opcode == 9 || opcode == 10 || opcode == 11 || opcode == 12 || \
 		opcode == 14 || opcode == 15)
 		return (2);
 	return (0);
+}
+
+int			if_encodage(int opcode)
+{
+	if (opcode == 1 || opcode == 9 || opcode == 12 || opcode == 15)
+		return (0);
+	else
+		return (1);
 }
 
 int			endof_instructions(int inst, int encod)
@@ -33,9 +41,13 @@ int			endof_instructions(int inst, int encod)
 	size = 0;
 	bin = ft_itoa(encod, 2);
 	bin = ft_strjoin(ft_chartostr('0', 8 - ft_strlen(bin)), bin, 3);
-	ft_printf("bin = %s\n", bin);
+	if (!ft_countchar(bin, '1') || !ft_countchar(bin, '0'))
+	{
+		size += check_encodage(inst);
+		return (size - 1);
+	}
 	while (i < 8)
-	{	
+	{
 		if (bin[i] == '0' && bin[i + 1] == '1')
 			size += 1;
 		else if (bin[i] == '1' && bin[i + 1] == '1')
@@ -54,21 +66,29 @@ void		cw_createnode(t_header2 *champion, int *tab, int size)
 	int			j;
 	int			end;
 
-	j = 0;
 	i = 0;
-	//ft_showtabint(tab, size, "inst_tab", 0);
 	while (i < size - 1)
 	{
+		j = 2;
 		end = 0;
-		//ft_printf("\n\n\n{red}----- Boucle  %d/%d ------{eoc}\n", i, size-1);
 		ft_printf("tab[%d] = %.2X -> %.2b -> %d\n", i, tab[i], tab[i], tab[i]);
 		new = (t_instnode*)malloc(sizeof(t_instnode));
-		new->inst = ft_inttabnew(size);
-		new->inst[i] = tab[i];
-		new->encodage = tab[i + 1];
 		end = endof_instructions(tab[i], tab[i + 1]);
-		ft_printf("size = %d\n", end);
-		i += end + 2;
+		new->inst = ft_inttabnew(end + 2);
+		new->inst[0] = tab[i];
+		if (if_encodage(tab[i]))
+			new->encodage = tab[i + 1];
+		new->size = end + 2;
+		new->inst[1] = tab[i + 1];
+		i += 2;
+		while (j < end + 2)
+		{
+			new->inst[j] = tab[i];
+			i++;
+			j++;
+		}
 		cw_pushback_inst(champion, new);
+		ft_showtabint(new->inst, end + 2, "inst_tab", 0);
+		ft_printf("encodage = %d, size = %d\n", new->encodage, new->size);
 	}
 }
