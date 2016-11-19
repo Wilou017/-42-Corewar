02f8dedc2e663db1d41331c56f842f5682d8f3d5
Aymeric MAITRE <amaitre@e3r3p8.42.fr>
2016-11-19 17:58:16 +0100

    print dynamic

--- src/vm_loop.c
+++ src/vm_loop.c
@@ -6,7 +6,7 @@
 /*   By: amaitre <amaitre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 17:40:38 by amaitre           #+#    #+#             */
-/*   Updated: 2016/11/19 17:56:26 by amaitre          ###   ########.fr       */
+/*   Updated: 2016/11/19 17:58:15 by amaitre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	cw_loop(t_cwdata *data)
 	while (1)
 	{
 		data->cur_cycle++;
+		ft_termcaps_screenclear();
 		ft_printf("Cycle courant = %d\n", data->cur_cycle);
 		print_map(data);
 		ft_termcaps_upline(MEM_SIZE/NB_OCT_LINE + 1);
