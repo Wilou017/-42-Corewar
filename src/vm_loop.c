8da122194fabc11866d2e4bd266f45e48d1d6bda
Aymeric MAITRE <amaitre@e3r3p8.42.fr>
2016-11-19 17:41:33 +0100

    Memory print function

--- src/vm_loop.c
+++ src/vm_loop.c
@@ -6,11 +6,13 @@
 /*   By: amaitre <amaitre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 17:40:38 by amaitre           #+#    #+#             */
-/*   Updated: 2016/11/19 17:40:41 by amaitre          ###   ########.fr       */
+/*   Updated: 2016/11/19 17:41:32 by amaitre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	cw_loop()
 {
-	print_map(data);
+	int i = 0;
+	while (i++ < 2)
+		print_map(data);
 }
