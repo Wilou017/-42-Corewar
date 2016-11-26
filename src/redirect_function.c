dce795688546768844f7c40b6ee298c377f2664b
Aymeric MAITRE <amaitre@e2r4p6.42.fr>
2016-11-26 17:32:04 +0100

    redir func

--- src/redirect_function.c
+++ src/redirect_function.c
@@ -6,7 +6,7 @@
 /*   By: amaitre <amaitre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 16:57:33 by amaitre           #+#    #+#             */
-/*   Updated: 2016/11/26 17:13:49 by amaitre          ###   ########.fr       */
+/*   Updated: 2016/11/26 17:31:52 by amaitre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void		redirect_function(t_cwdata *data, t_func func, t_process *proc)
 {
 	if (func == LIVE)
-		cw_fc_live(data, proc);
+		ft_printf("LIVE !\n");
 	else if (func == LD)
-		cw_fc_ld(data, proc);
+		ft_printf("LD !\n");
 }
