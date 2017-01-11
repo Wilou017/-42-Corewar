94ea82921da085ad73b898ab3b36ca4b660dacbe
Aymeric MAITRE <amaitre@e2r5p19.42.fr>
2017-01-11 20:56:48 +0100

    compil done

--- src/norme_func.c
+++ src/norme_func.c
@@ -6,7 +6,7 @@
 /*   By: amaitre <amaitre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 17:17:46 by amaitre           #+#    #+#             */
-/*   Updated: 2017/01/10 16:59:49 by amaitre          ###   ########.fr       */
+/*   Updated: 2017/01/11 20:35:35 by amaitre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int		cw_norme1(t_reedstruct reed, t_header *champion)
 	return (0);
 }
 
-void	vm_print(t_cwdata *data, t_process proc, int loca)
+void	vm_print(t_cwdata *data, t_process *proc, int loca)
 {
 	int i;
 
 	ft_termcaps_poscurs(loca / NB_OCT_LINE + 3, (loca % NB_OCT_LINE) * 3 + 4);
-	ft_printf("{%s}", color[ABS(proc->id_champ)]);
+	ft_printf("{%s}", data->color[ABS(proc->id_champ)]);
 	i = -1;
 	while (++i < 3)
 		ft_printf("%.2X", data->mem[loca + i]);
