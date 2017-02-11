04bf35ff42927072c7ff33bfe9313dfc156582a0
Aymeric MAITRE <amaitre@e2r1p5.42.fr>
2017-02-11 23:17:15 +0100

    GodFather last version

--- README.md
+++ README.md
@@ -41,3 +41,36 @@ http://aymericdev.fr/img/GITHUB_COREWAR_01.png
 
 http://aymericdev.fr/img/GITHUB_COREWAR_00.png
 <img src="http://aymericdev.fr/img/GITHUB_COREWAR_00.png"/>
+
+--- header
+
+magic octet : 4 octets (unsigned int)
+prog_name : 128 octets + 1 padding(int value == 0)
+prog_size : 4 octets (unsigned int)
+prog_comment : 2048 octets + 1 padding (int value == 0)
+
+--- instructions
+
+architecture :
+		- opcode 1 octet
+		- octet d'encodage 1 octet
+		- parametres varie avec l'octet d'encodage
+
+exemple instructions :
+
+		- 0b opcode (0b == 11)
+		- 68 binaire == 01 10 10 00
+		- 01 00 45 00 01
+
+https://docs.google.com/spreadsheets/d/1pFwSCne-mh-u5ZLsjZS8VI9QvecYk-gWTyNaPstjpLE/edit#gid=0
+
+
+function that modifie carry --> -ld
+								-add
+								-sub
+								-and
+								-or
+								-xor
+								-lld
+								-lldi
+
--- cor/GodFather.s
+++ cor/GodFather.s
@@ -1,90 +1,103 @@
 .name "GodFather"
 .comment "I'm gonna make him an offer he can't refuse"
 
-
 fork		%:initfork
 ld			%0, r16
 base:
+	live	%4294967295
+	fork	%:shot
 	sti		r1, %:initfork, %1
 	sti		r1, %:initfork, %9
 	sti		r1, %:initfork, %17
-	sti		r1, %:usineforkeur, %1
-	sti		r1, %:usineforkeur, %9
-	sti		r1, %:usineforkeur, %17
-	sti		r1, %:usineforkeur, %25
-	sti		r1, %:usineforkeur, %33
-	sti		r1, %:usineforkeur, %41
 	sti		r1, %:wall, %1
+	sti		r1, %:usine, %1
+	sti		r1, %:usine, %9
+	sti		r1, %:usine, %17
+	sti		r1, %:usine, %25
+	sti		r1, %:usine, %33
 	sti		r1, %:alive, %1
 	sti		r1, %:attforker, %1
 	sti		r1, %:attforker, %9
-	sti		r1, %:forkeur, %1
-	sti		r1, %:forkeur3, %1
-	sti		r1, %:forkeur4, %1
-	sti		r1, %:wall2, %1
-	sti		r1, %:wall2, %9
-	sti		r1, %:wall2, %17
-	fork	%:wall2
+	sti		r1, %:attforker, %17
+	sti		r1, %:shot, %1
+	sti		r1, %:initshot1, %1
+	sti		r1, %:initshot2, %1
+	sti		r1, %:salve, %1
+	sti		r1, %:salve, %9
+	sti		r1, %:salve, %17
+	sti		r1, %:salve3, %1
+	sti		r1, %:salve4, %1
+	sti		r1, %:salve5, %1
 
 initfork:
 	live	%2863311530
 	fork	%:wall
 	live	%2863311530
-	fork	%:usineforkeur
+	fork	%:usine
 	live	%2863311530
 	fork	%:initfork
+	ld		%0, r16
 	zjmp	%:initfork
 
 wall:
 	live	%2863311530
-	sti		r16, %:base, %120
-	sti		r16, %:base, %116
-	sti		r16, %:base, %112
-	sti		r16, %:base, %108
-	sti		r16, %:base, %104
-	sti		r16, %:base, %100
-	sti		r16, %:base, %96
-	sti		r16, %:base, %92
-	sti		r16, %:base, %88
-	sti		r16, %:base, %84
-	sti		r16, %:base, %80
-	sti		r16, %:base, %76
-	sti		r16, %:base, %72
-	sti		r16, %:base, %68
-	sti		r16, %:base, %64
-	sti		r16, %:base, %60
-	sti		r16, %:base, %56
-	sti		r16, %:base, %52
-	sti		r16, %:base, %48
-	sti		r16, %:base, %44
-	sti		r16, %:base, %40
-	sti		r16, %:base, %36
-	sti		r16, %:base, %32
-	sti		r16, %:base, %28
-	sti		r16, %:base, %24
-	sti		r16, %:base, %20
-	sti		r16, %:base, %16
-	sti		r16, %:base, %12
-	sti		r16, %:base, %8
-	sti		r16, %:base, %4
-	sti		r16, %:base, %0
+	# st			r16, -34
+	st			r16, -43
+	# st			r16, -52
+	st			r16, -61
+	# st			r16, -70
+	st			r16, -79
+	# st			r16, -88
+	st			r16, -97
+	# st			r16, -106
+	st			r16, -115
+	# st			r16, -124
+	st			r16, -133
+	# st			r16, -142
+	st			r16, -151
+	# st			r16, -160
+	st			r16, -169
+	# st			r16, -178
+	st			r16, -187
+	# st			r16, -196
+	st			r16, -205
+	# st			r16, -214
+	st			r16, -223
+	# st			r16, -232
+	st			r16, -241
+	# st			r16, -250
+	st			r16, -259
+	# st			r16, -268
+	st			r16, -277
+	# st			r16, -286
+	st			r16, -295
+	# st			r16, -304
+	st			r16, -313
+	# st			r16, -322
+	st			r16, -331
+	# st			r16, -340
+	st			r16, -349
+	# st			r16, -358
+	st			r16, -367
+	# st			r16, -376
+	st			r16, -385
+	st			r16, -394
+	# ld		%0, r16
 	zjmp	%:wall
 
-usineforkeur:
+usine:
 	live	%2863311530
 	fork	%:wall
 	live	%2863311530
-	fork	%:wall2
-	live	%2863311530
-	fork	%:forkeur
+	fork	%:shot
 	live	%2863311530
 	fork	%:attforker
 	live	%2863311530
-	fork	%:usineforkeur
+	fork	%:usine
 	live	%2863311530
-	ld		%0, r16 # verifier dans la vrai
+	ld		%0, r16
 	fork	%:alive
-	zjmp	%:usineforkeur
+	zjmp	%:usine
 
 alive:
 	live	%2863311530
@@ -94,91 +107,92 @@ attforker:
 	live	%2863311530
 	fork	%:attforker
 	live	%2863311530
-	fork	%:forkeur
+	fork	%:shot
+	live	%2863311530
+	fork	%:salve
+	ld		%0, r16
 	zjmp	%:attforker
 
-forkeur:
+shot:
 	live	%2863311530
-	fork	%:forkeur4
+	fork	%:initshot2
 
-forkeur3:
+initshot1:
 	live	%2863311530
-	fork	%:init3
-
-init1:
-	ld      %150994955, r2
-	ld      %288, r3
+	ld      %190054927, r2
+	ld      %506, r3
 	ld		%0, r15
-	zjmp    %:print
+	zjmp    %:printshot
 
-forkeur4:
+initshot2:
 	live	%2863311530
-	fork	%:init4
-
-init2:
-	ld      %1409421059, r2
-	ld      %292, r3
+	# ld      %50921472, r2 || jump 0
+	ld      %50921973, r2
+	ld      %510, r3
 	ld		%0, r15
-	zjmp    %:print
+	zjmp    %:printshot
 
-init3:
-	#		33554431
-	ld      %33554431, r2
-	ld      %296, r3
-	ld		%0, r15
-	zjmp    %:print
-
-init4:
-	# ld      %590105, r2
-	ld      %4278780185, r2
-	ld      %300, r3
-	ld		%0, r15
-	zjmp    %:print
-
-print:
+printshot:
 	sti		r2, r15, r3
-	zjmp	%286
+	zjmp	%501 # shot1 - 5
 
-wall2:
+salve:
 	live	%2863311530
-	fork	%96
+	fork	%:salve
 	live	%2863311530
-	fork	%:wall2
+	fork	%:salve4
 	live	%2863311530
-	ld		%655358, r13
-	sti		r13, %:end, %4
-	sti		r13, %:end, %8
-	sti		r13, %:end, %12
-	sti		r13, %:end, %16
-	sti		r13, %:end, %20
-	sti		r13, %:end, %24
-	sti		r13, %:end, %28
-	sti		r13, %:end, %32
-	sti		r13, %:end, %36
-	sti		r13, %:end, %40
-	sti		r13, %:end, %44
-	sti		r13, %:end, %48
-	sti		r13, %:end, %52
-	sti		r13, %:end, %56
-	sti		r13, %:end, %60
-	sti		r13, %:end, %64
-	sti		r13, %:end, %68
-	sti		r13, %:end, %72
-	sti		r13, %:end, %76
-	sti		r13, %:end, %80
-	sti		r13, %:end, %84
-	sti		r13, %:end, %88
-	sti		r13, %:end, %92
-	sti		r13, %:end, %96
-	sti		r13, %:end, %100
-	sti		r13, %:end, %104
-	sti		r13, %:end, %108
-	sti		r13, %:end, %112
-	sti		r13, %:end, %116
-	sti		r13, %:end, %120
-	sti		r13, %:end, %124
-	sti		r13, %:end, %128
-	ld		%0, r16 # verifier dans la vrai
-	zjmp	%:wall2
+	fork	%:salve5
 
-end:
+salve3:
+	live	%2863311530
+	fork	%:initsalve3
+
+initsalve1:
+	add		r16, r1, r2
+	ld      %22, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+salve4:
+	live	%2863311530
+	fork	%:initsalve4
+
+initsalve2:
+	lld      %1409421059, r2
+	ld      %17, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+salve5:
+	live	%2863311530
+	fork	%:initsalve6
+
+initsalve5:
+	add		r16, r1, r2
+	ld      %511, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+initsalve3:
+	# add		r16, r1, r2
+	lld      %27962026, r2
+	ld      %21, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+initsalve4:
+	lld      %2852716550, r2
+	ld      %25, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+initsalve6:
+	lld      %150994955, r2
+	ld      %13, r3
+	ld		%0, r15
+	zjmp    %:printsalve
+
+printsalve:
+	sti		r2, r15, r3
+	zjmp	%10
